d0f3f51850c7b2ac05e4a346075fcdf90cd523e9
Denis Khrapov <khrapov@haulmont.com>
2019-10-30 18:32:50 +0400

    final

---
+++ demo-spring-boot/src/main/java/com/haulmont/demospringboot/healthindicators/CustomHealthIndicator.java
@@ -0,0 +1,21 @@
+package com.haulmont.demospringboot.healthindicators;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Component
+public class CustomHealthIndicator implements HealthIndicator {
+
+    @Override
+    public Health health() {
+
+        if (new Random().nextBoolean()) {
+            return Health.down().build();
+        }
+
+        return Health.up().build();
+    }
+}
